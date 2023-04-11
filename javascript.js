7916df4eb34ea2885b3f1ed2c08f9c2f75e6e381
Hiyiragi <128255052+Hiyiragi@users.noreply.github.com>
2023-04-11 05:33:06 +0200

    added web component to buttons in option sidebar

--- javascript.js
+++ javascript.js
@@ -11,6 +11,20 @@ class AppSidebarbutton extends HTMLElement {
         const number = this.getAttribute("unreadCount")
         this.shadowRoot.querySelector(".text").innerText = text;
         this.shadowRoot.querySelector(".number").innerText = number;
+        
+        
+        const fixed = this.getAttribute("fixed");
+        if (fixed==="true") {
+            this.shadowRoot.querySelector(".options-body").style.backgroundColor = `#C2E6FF`;
+        }
+        
+        if (Number(number)>0) {
+            this.setAttribute("boldText", "true");
+            console.log(this)
+        }
+        
+        const bold = this.getAttribute("boldText");
+        if (bold==="true") this.shadowRoot.querySelector(".text").style.fontWeight = `bold`;
     }
 }
 
